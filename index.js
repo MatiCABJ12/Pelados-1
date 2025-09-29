f67f31bbb29481dc65c647b42d17942d1e7b1de6
MatiCABJ12 <50233785@est.ort.edu.ar>
2025-09-29 10:30:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,12 @@ import {
   seleccionarSabores,
 } from "./dataEntry.js";
 
-// Cargar sabores y productos
-// COMPLETEN USTEDES
-let sabores;
-let productos;
+let sabores = JSON.parse (fs.readFileSync("data/sabores.json", "utf-8"));
+let productos = JSON.parse (fs.readFileSync("data/productos.json", "utf-8"));;
 
-// Ingresar cliente
 let cliente = ingresarCliente();
-// Elegir producto
 let producto = seleccionarProducto(productos);
-// Elegir cantidad de gustos
 let gustos = seleccionarCantidadGustos(producto.maxGustos);
-// Elegir sabores
 let saboresElegidos = seleccionarSabores(sabores, gustos);
 
 // Guardar pedido
